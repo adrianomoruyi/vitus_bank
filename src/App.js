4ba07e52006a2903273605fce7ed344c97c888d8
Adrian Omoruyi <78651386+adrianomoruyi@users.noreply.github.com>
2024-11-23 16:25:22 -0500

    Completed front page (for the query button)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
         <h2>Hello, there. Welcome to <strong>Vitus.</strong></h2>
         <Button text='Register Today!'></Button>
       </div>
+      <Button style={{
+          position: 'absolute',
+          bottom: '30px',
+          left: '30px',}} text='Query Page'></Button>
     </div>
   );
 }
